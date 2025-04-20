6d51cee84015e017a8173517ef4d7cd9cefdbb41
Victor Leite <victor_leite@outlook.com.br>
2025-04-20 16:24:00 -0300

    initial commit

---
+++ src/main/java/com/fiap/autohub/autohub_vehicles_api/AutohubVehiclesApiApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.autohub.autohub_vehicles_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutohubVehiclesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutohubVehiclesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/autohub/autohub_vehicles_api/AutohubVehiclesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.autohub.autohub_vehicles_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutohubVehiclesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
